c63a93f5758d7cf8cc28ed19d413cf4981f27960
Andres19011 <agallegogarcia030@gmail.com>
2025-08-28 23:33:57 -0500

    fix: Error 500 en las personas, por tipo de dato.

--- models.py
+++ models.py
@@ -10,14 +10,14 @@ class Paciente(Base):
 
 class Medico(Base):
     __tablename__ = 'medicos'
-    idMedico = Column(Integer, primary_key=True, index=True)
+    idMedico = Column(String, primary_key=True, index=True)
     especializacion = Column(String, index=True)
     nombreMedico = Column(String, index=True)
     correoMedico = Column(String, index=True)
 
 class Enfermera(Base):
     __tablename__ = 'enfermeras'
-    idEnfermera = Column(Integer, primary_key=True, index=True)
+    idEnfermera = Column(String, primary_key=True, index=True)
     nombreEnfermera = Column(String, index=True)
     area = Column(String, index=True)
     correoEnfermera = Column(String, index=True)
@@ -26,7 +26,7 @@ class AgendarCita(Base):
     __tablename__ = 'agendar_cita'
     idCita = Column(Integer, primary_key=True, index=True)
     idPaciente = Column(String, ForeignKey('pacientes.idPaciente'))
-    idMedico = Column(Integer, ForeignKey('medicos.idMedico'))
+    idMedico = Column(String, ForeignKey('medicos.idMedico'))
     fechaAgendamiento = Column(Date, index=True)
     fechaEmision = Column(DateTime, default=datetime.now)  # Aquí se replica GETDATE()
     motivoConsulta = Column(String, index=True)
@@ -35,9 +35,9 @@ class Diagnostico(Base):
     __tablename__ = 'diagnostico'
     idDiagnostico = Column(Integer, primary_key=True, index=True)
     idCita = Column(Integer, ForeignKey('agendar_cita.idCita'))
-    idMedico = Column(Integer, ForeignKey('medicos.idMedico'))
+    idMedico = Column(String, ForeignKey('medicos.idMedico'))
     idPaciente = Column(String, ForeignKey('pacientes.idPaciente'))
-    idEnfermera = Column(Integer, ForeignKey('enfermeras.idEnfermera'))
+    idEnfermera = Column(String, ForeignKey('enfermeras.idEnfermera'))
     fechaEmision = Column(Date, index=True)  # Aquí se replica GETDATE()
     fechaDiagnostico = Column(DateTime, default=datetime.now)  # Aquí se replica GETDATE()
     descripcionDiagnostico = Column(String, index=True)
