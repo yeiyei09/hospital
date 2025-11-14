b33df61920c81b7376d2441d59bebe750bf14e17
yeiyei09 <yeffergomez02@gmail.com>
2025-11-14 00:16:01 -0500

    añadir puerto frontend

--- main.py
+++ main.py
@@ -47,9 +47,12 @@ app = FastAPI(
     ],
 )
 
+origins = [
+    "http://localhost:4200",  # Frontend Angular
+]
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["*"],
+    allow_origins=origins,
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
